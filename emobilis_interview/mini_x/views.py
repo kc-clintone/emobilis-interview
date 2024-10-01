69009c79588f1860d547308b9847effb6df192be
kc-clintone <kayseeclintone@gmail.com>
2024-10-01 08:11:10 +0000

    making first migrations

--- emobilis_interview/mini_x/views.py
+++ emobilis_interview/mini_x/views.py
@@ -1,6 +1,23 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, logout
 from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect
+from .models import Post
+# from django.contrib.auth.decorators import login_required
+
+def create_post(request):
+    if request.method == 'POST':
+        content = request.POST.get('content')
+        Post.objects.create(content=content, author=request.user)
+        return redirect('home')
+    return render(request, 'create_post.html')
+
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'home.html', {'posts': posts})
+
+
+
 
 def signup(request):
     if request.method == 'POST':
