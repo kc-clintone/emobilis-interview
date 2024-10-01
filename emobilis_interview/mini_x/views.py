30be351ff0774e020c73b19302481d1355e5e876
kc-clintone <kayseeclintone@gmail.com>
2024-10-01 07:59:02 +0000

    making views and models

--- emobilis_interview/mini_x/views.py
+++ emobilis_interview/mini_x/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+def login_view(request):
+    # Implement login logic
+    pass
+def logout_view(request):
+    # Implement logout logic
+    pass
+
