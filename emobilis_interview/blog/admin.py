8b2936b1d37e9bce2b3854cd660bd4cd0967e219
kc-clintone <kayseeclintone@gmail.com>
2024-10-01 11:26:57 +0000

    tiding up and minor changes

--- emobilis_interview/blog/admin.py
+++ emobilis_interview/blog/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
-
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'status','created_on')
     list_filter = ("status",)
